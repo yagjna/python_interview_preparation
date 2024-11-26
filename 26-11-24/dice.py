629288ab9a4455d8df2a4e98ad035adbe200636d
yagjna <yagjna1101@gmail.com>
2024-11-26 15:34:40 +0530

    characters, dice rolling programmes

---
+++ 26-11-24/dice.py
@@ -0,0 +1,18 @@
+import random
+
+def roll_dice():
+    die1 = random.randint(1, 6)
+    die2 = random.randint(1, 6)
+    print("Die 1:", die1)
+    print("die2",die2)
+    print("Total:", die1 + die2)
+
+roll_dice()
+~
+~
+~
+
+output :
+    Die 1: 4
+die2 3
+Total: 7
