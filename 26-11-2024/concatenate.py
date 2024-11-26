7ea23530b8b41b3199a256b49007a8a8748376a9
yagjna <yagjna1101@gmail.com>
2024-11-26 20:49:06 +0530

    split and conactenate

---
+++ 26-11-2024/concatenate.py
@@ -0,0 +1,9 @@
+def concatenate(lst1):
+    return ' '.join(lst1)
+lst1 = ['python', 'is', 'fun']
+result = concatenate(lst1)
+print('concatenated string is -- {}'.format(result))
+
+
+output: 
+      concatenated string is --python is fun
---
+++ 26-11-2024/split.py
@@ -0,0 +1,10 @@
+
+def split(sentence):
+    lst1 = sentence.split(' ')
+    return lst1
+sentence = 'python is a language'
+lst1 = split(sentence)
+print("the words are {} ".format(lst1))
+
+
+output :the words are ['python', 'is', 'a', 'language']
