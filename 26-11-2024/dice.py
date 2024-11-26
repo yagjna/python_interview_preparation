1254e664150b166260f48fc42e68501ca53ab77d
yagjna <yagjna1101@gmail.com>
2024-11-26 14:43:00 +0530

    dice rolling

---
+++ 26-11-2024/dice.py
@@ -0,0 +1,10 @@
+import random
+
+def roll_dice():
+    die1 = random.randint(1, 6)
+    die2 = random.randint(1, 6)
+    print("Die 1:", die1)
+    print("die2",die2)
+    print("Total:", die1 + die2e
+
+roll_dice()
