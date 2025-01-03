c0742758cecfb6e029afe040bbef95b3b41675ce
yagjna <yagjna1101@gmail.com>
2025-01-03 22:35:51 +0530

    file added

---
+++ 03-01-2025/factorial.py
@@ -0,0 +1,16 @@
+
+def factorial(number):
+    
+    fact = 1
+
+    for x in range(1, number+1):
+
+        fact = fact * x
+
+    return fact
+number = 5
+print(factorial(number))
+
+#output:
+120
+
